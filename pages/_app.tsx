60b66b3928d6d664af3ae297c4c5a8049a3c0f82
mustythefirst <m.adde.osman@gmail.com>
2023-05-03 12:38:34 +0100

    https://github.com/mustythefirst/nft-mint-alchemy2.git

---
+++ pages/_app.tsx
@@ -0,0 +1,85 @@
+import '../styles/global.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+} from '@rainbow-me/rainbowkit';
+import { argentWallet, trustWallet } from '@rainbow-me/rainbowkit/wallets';
+import { createClient, configureChains, WagmiConfig } from 'wagmi';
+import { Chain } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const sepolia: Chain = {
+  id: 11155111,
+  name: 'Sepolia',
+  network: 'sepolia',
+  nativeCurrency: { name: 'Sepolia Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    alchemy: { http: ['https://eth-sepolia.g.alchemy.com/v2/m-ENVUngFO4we3LF-C5-Lrp3ZqmMgCAO'] },
+    default: { http: ['https://eth-sepolia.g.alchemy.com/v2/m-ENVUngFO4we3LF-C5-Lrp3ZqmMgCAO'] },
+    public: { http: ['https://rpc.ankr.com/eth_rinkeby'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'Etherscan', url: 'https://sepolia.etherscan.io/' },
+    default: { name: 'Etherscan', url: 'https://sepolia.etherscan.io/' },
+  },
+  contracts: {
+    ensRegistry: {
+      address: '0x699f6a90E8F55A73aE2E6B8Fdc5f2247D3785d03',
+    },
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 10299530,
+    },
+  },
+  testnet: true,
+};
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [sepolia],
+  [publicProvider()]
+);
+
+const projectId = 'YOUR_PROJECT_ID';
+
+const { wallets } = getDefaultWallets({
+  appName: 'RainbowKit Mint NFT Demo',
+  projectId,
+  chains,
+});
+
+const demoAppInfo = {
+  appName: 'RainbowKit Mint NFT Demo',
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Other',
+    wallets: [
+      argentWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
